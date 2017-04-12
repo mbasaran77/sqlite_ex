06f4eeae9413eae11b098201286e9e1ac7150746
mbasaran77 <mbasaran77@gmail.com>
2017-04-12 18:25:31 +0300

    postgreSql -0

--- dene_3.py
+++ dene_3.py
@@ -1,23 +1,25 @@
-import sqlite3
 import psycopg2
-conn = sqlite3.connect('lite.db')
-cur = conn.cursor()
-cur.execute('pragma foreign_keys=ON')
+import psycopg2
+# conn = psycopg2.connect('lite.db')
+# cur = conn.cursor()
+# cur.execute('pragma foreign_keys=ON')
 
 
 
 def create_table():
-    conn = sqlite3.connect('lite.db')
+    conn = psycopg2.connect("dbname = 'database1' user = 'postgres' password = '1' host = 'localhost' port = '5432' ")
     cur = conn.cursor()
-    cur.execute('pragma foreign_keys=ON')
+    # cur.execute('pragma foreign_keys=ON')
 
+    # cur.execute("CREATE TABLE IF NOT EXISTS kitap(kitap_id INTEGER PRIMARY KEY NOT NULL, kitap_name TEXT, kitap_yazar INTEGER NOT NULL,"
+    # "adet INTEGER, fiyat REAL,FOREIGN KEY (kitap_yazar) REFERENCES yazar_tablo(yazar_id))")
     cur.execute("CREATE TABLE IF NOT EXISTS kitap(kitap_id INTEGER PRIMARY KEY NOT NULL, kitap_name TEXT, kitap_yazar INTEGER NOT NULL,"
-    "adet INTEGER, fiyat REAL,FOREIGN KEY (kitap_yazar) REFERENCES yazar_tablo(yazar_id))")
+    "adet INTEGER, fiyat REAL)")
     conn.commit()
     conn.close()
 
 def view_data(table_name):
-    conn = sqlite3.connect('lite.db')
+    conn = psycopg2.connect('lite.db')
     cur = conn.cursor()
     cur.execute("SELECT * FROM {tn}".format(tn=table_name))
     # cur.execute("SELECT * FROM yazar_ad")
@@ -26,7 +28,7 @@ def view_data(table_name):
     return rows
 
 def insert_data(kitap_id,kitap_adi,yazar_id,adet,fiyat):
-    conn = sqlite3.connect('lite.db')
+    conn = psycopg2.connect('lite.db')
     cur = conn.cursor()
     cur.execute('pragma foreign_keys=ON')
 
@@ -36,40 +38,40 @@ def insert_data(kitap_id,kitap_adi,yazar_id,adet,fiyat):
     conn.close()
 
 def delete_data(data):
-    conn = sqlite3.connect('lite.db')
+    conn = psycopg2.connect('lite.db')
     cur = conn.cursor()
     cur.execute("DELETE  FROM kitap WHERE kitap_name=? ",(data,))
     conn.commit()
     conn.close()
 
 def update_data(kitap_adi,adet,fiyat):
-    conn = sqlite3.connect('lite.db')
+    conn = psycopg2.connect('lite.db')
     cur = conn.cursor()
     cur.execute("UPDATE kitap SET adet = ?, fiyat = ?  WHERE kitap_name= ?",(adet,fiyat,kitap_adi))
     conn.commit()
     conn.close()
 def create_table_yazar():
-    conn = sqlite3.connect('lite.db')
+    conn = psycopg2.connect('lite.db')
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS yazar_tablo(yazar_id INTEGER PRIMARY KEY NOT NULL, yazar_ad TEXT)")
     conn.commit()
     conn.close()
 
 def delete_table(table_name):
-    conn = sqlite3.connect('lite.db')
+    conn = psycopg2.connect('lite.db')
     cur = conn.cursor()
     cur.execute("DROP TABLE {tn}".format(tn=table_name))
     conn.commit()
     conn.close()
 def rec_yazar(yazar_id, yazar_ad):
-    conn = sqlite3.connect('lite.db')
+    conn = psycopg2.connect('lite.db')
     cur = conn.cursor()
     cur.execute("INSERT INTO yazar_tablo VALUES(?,?) ",(yazar_id,yazar_ad))
     conn.commit()
     conn.close()
 
 def yazar_kitap_sorgu(yazar_adi):
-    conn = sqlite3.connect('lite.db')
+    conn = psycopg2.connect('lite.db')
     cur = conn.cursor()
     cur.execute("SELECT yazar_id FROM yazar_tablo WHERE yazar_ad =(?) ",(yazar_adi,))
     yazar_id = cur.fetchone()
@@ -94,7 +96,7 @@ create_table()
 # delete_data("vadideki zambak")
 # rec_yazar(2,"yasar kemal")
 # rec_yazar(3,"nihal atsiz")
-print(view_data("kitap"))
-print(view_data("yazar_tablo"))
-yazar_kitap_sorgu('yasar kemal')
-yazar_kitap_sorgu('nihal atsiz')
+# print(view_data("kitap"))
+# print(view_data("yazar_tablo"))
+# yazar_kitap_sorgu('yasar kemal')
+# yazar_kitap_sorgu('nihal atsiz')
